364121b0e08f1e5d8e81bfb1b5ac13dce10c2f71
shreyak-29 <shreyakathe2904@example.com>
2024-02-09 23:29:29 +0530

    .

---
+++ variables/let.js
@@ -0,0 +1,4 @@
+let num = 10;
+console.log(num);
+let num1 = 20;
+console.log(num1);
---
+++ variables/var.js
@@ -0,0 +1,13 @@
+var num = 10;
+console.log(num);
+var num = 15;
+console.log(num);
+
+var name='Shreya';
+console.log(name);
+
+name='Sayli';
+console.log(name);
+
+var name= 'Kathe';
+console.log(name);
